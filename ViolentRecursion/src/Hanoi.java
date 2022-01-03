32314ee3d54f0def55aab288449da307d8c0b555
yyf <yyfjlyx@163.com>
2022-01-03 15:46:52 +0800

    暴力递归--汉诺塔

--- ViolentRecursion/src/Hanoi.java
+++ ViolentRecursion/src/Hanoi.java
@@ -1,3 +1,87 @@
 public class Hanoi {
+    public void hanoi1(int n) {
+        moveLeftToRight(n);
+    }
 
+    private void moveLeftToRight(int n) {
+        if (n == 1) {
+            System.out.println("move " + n + " from left to right!");
+            return;
+        }
+
+        moveLeftToMid(n - 1);
+        System.out.println("move " + n + " from left to right!");
+        moveMidToRight(n - 1);
+    }
+
+
+    private void moveLeftToMid(int n) {
+        if (n == 1) {
+            System.out.println("move " + n + " from left to mid!");
+            return;
+        }
+
+        moveLeftToRight(n - 1);
+        System.out.println("move " + n + " from left to mid!");
+        moveRightToMid(n - 1);
+    }
+
+    private void moveRightToMid(int n) {
+        if (n == 1) {
+            System.out.println("move " + n + " from right to mid!");
+            return;
+        }
+
+        moveRightToLeft(n - 1);
+        System.out.println("move " + n + " from right to mid!");
+        moveLeftToMid(n - 1);
+    }
+
+    private void moveRightToLeft(int n) {
+        if (n == 1) {
+            System.out.println("move " + n + " from right to left!");
+            return;
+        }
+
+        moveRightToMid(n - 1);
+        System.out.println("move " + n + " from right to left!");
+        moveMidToLeft(n - 1);
+    }
+
+    private void moveMidToRight(int n) {
+        if (n == 1) {
+            System.out.println("move " + n + " from mid to right!");
+            return;
+        }
+
+        moveMidToLeft(n - 1);
+        System.out.println("move " + n + " from mid to right!");
+        moveLeftToRight(n - 1);
+    }
+
+    private void moveMidToLeft(int n) {
+        if (n == 1) {
+            System.out.println("move " + n + " from mid to left!");
+            return;
+        }
+
+        moveMidToRight(n - 1);
+        System.out.println("move " + n + " from mid to left!");
+        moveRightToLeft(n - 1);
+    }
+
+    public void hanoi2(int n) {
+        process(n, "left", "right", "mid");
+    }
+
+    public void process(int n, String left, String right, String mid) {
+        if (n == 1) {
+            System.out.println("move " + n + " from " + left + " to " + right + "!");
+            return;
+        }
+
+        process(n - 1, left, mid, right);
+        System.out.println("move " + n + " from " + left + " to " + right + "!");
+        process(n - 1, mid,right, left);
+    }
 }
--- ViolentRecursion/src/Test.java
+++ ViolentRecursion/src/Test.java
@@ -1,8 +1,19 @@
 public class Test {
     public static void main(String[] args) {
+        testHanoi();
 
-        int[] weights = { 3, 2, 4, 7, 3, 1, 7 };
-        int[] values = { 5, 6, 3, 19, 12, 4, 2 };
+    }
+
+    private static void testHanoi() {
+        Hanoi hanoi = new Hanoi();
+//        hanoi.hanoi2(3);
+        hanoi.hanoi2(30);
+
+    }
+
+    private static void testKnapsack() {
+        int[] weights = {3, 2, 4, 7, 3, 1, 7};
+        int[] values = {5, 6, 3, 19, 12, 4, 2};
         int bag = 15;
         Knapsack knapsack = new Knapsack();
         System.out.println(knapsack.knapsack(weights, values, bag));
